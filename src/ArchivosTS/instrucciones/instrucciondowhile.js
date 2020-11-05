ee0db7e0ed0af6245e6d8a8dc76036d329b35b3f
salaz310 <lrso.10@hotmail.com>
2020-11-04 20:14:10 -0600

    Break y Continue en Do-While

--- src/ArchivosTS/instrucciones/instrucciondowhile.js
+++ src/ArchivosTS/instrucciones/instrucciondowhile.js
@@ -15,13 +15,12 @@ var instrucciondowhile = /** @class */ (function () {
     instrucciondowhile.prototype.traducir = function (ambito) {
         var generador = generacion_1.generacion.getGenerador();
         var ambito_dowhile = new entorno_1["default"]("DO-WHILE", tipo_1.tipo_ambito.LOCAL, ambito);
-        //PENDIENTE
-        /**REVISAR LO DEL BREAK O CONTINUE */
-        //ambito_dowhile.etq_continue= this.condicion.etq_true= generador.generarEtiqueta();
-        //ambito_dowhile.etq_break= this.condicion.etq_false= generador.generarEtiqueta();
         generador.agregarComentarios("INICIO- DO_WHILE");
         var etiqueta_inicio = generador.generarEtiqueta();
+        var etiqueta_break = generador.generarEtiqueta();
         generador.agregarEtiqueta(etiqueta_inicio);
+        ambito_dowhile.etq_continue = etiqueta_inicio;
+        ambito_dowhile.etq_break = etiqueta_break;
         for (var i = 0; i < this.lista_do.length; i++) {
             var retorno_ins = this.lista_do[i].traducir(ambito_dowhile);
         }
@@ -31,6 +30,7 @@ var instrucciondowhile = /** @class */ (function () {
             generador.agregarEtiqueta(retorno_condicion.etiquetastrue);
             generador.agregarGoTo(etiqueta_inicio);
             generador.agregarEtiqueta(retorno_condicion.etiquetasfalse);
+            generador.agregarEtiqueta(etiqueta_break);
             generador.agregarComentarios("FIN- DO_WHILE");
         }
         else {
--- src/ArchivosTS/instrucciones/instrucciondowhile.ts
+++ src/ArchivosTS/instrucciones/instrucciondowhile.ts
@@ -24,14 +24,13 @@ export class instrucciondowhile implements instruccion{
 
         const generador= generacion.getGenerador();
         const ambito_dowhile= new entorno("DO-WHILE",tipo_ambito.LOCAL,ambito);
-        //PENDIENTE
-        /**REVISAR LO DEL BREAK O CONTINUE */
-        //ambito_dowhile.etq_continue= this.condicion.etq_true= generador.generarEtiqueta();
-        //ambito_dowhile.etq_break= this.condicion.etq_false= generador.generarEtiqueta();
 
         generador.agregarComentarios("INICIO- DO_WHILE");
         let etiqueta_inicio= generador.generarEtiqueta();
+        let etiqueta_break= generador.generarEtiqueta();
         generador.agregarEtiqueta(etiqueta_inicio);
+        ambito_dowhile.etq_continue=etiqueta_inicio;
+        ambito_dowhile.etq_break= etiqueta_break;
         for (let i = 0; i < this.lista_do.length; i++) {
             let retorno_ins= this.lista_do[i].traducir(ambito_dowhile)
         }
@@ -41,6 +40,7 @@ export class instrucciondowhile implements instruccion{
             generador.agregarEtiqueta(retorno_condicion.etiquetastrue);
             generador.agregarGoTo(etiqueta_inicio);
             generador.agregarEtiqueta(retorno_condicion.etiquetasfalse);
+            generador.agregarEtiqueta(etiqueta_break);
             generador.agregarComentarios("FIN- DO_WHILE");
         }else{
 
@@ -53,7 +53,6 @@ export class instrucciondowhile implements instruccion{
             }));
 
         }
-
 
     }
 
